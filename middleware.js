d8e196921a522f0807042e797c16339c0f5b1481
N.Aishwarya <n.aishwarya1261@gmail.com>
2025-11-15 21:46:40 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const response = NextResponse.next();
+
+  response.headers.set(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self'; frame-ancestors 'none';"
+  );
+
+  response.headers.set("X-Frame-Options", "DENY");
+  response.headers.set("X-XSS-Protection", "1; mode=block");
+  response.headers.set("X-Content-Type-Options", "nosniff");
+
+  return response;
+}
+
